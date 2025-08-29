572ee87a1eae95e8dd15f093975b2161a699bb6e
honeybees00 <Cortizmason@outlook.com>
2025-08-28 21:04:51 -0400

    added id class for buttons

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 setProperty("WaterAppHeader", "color", "teal");
 setProperty("subHeader", "color", "Purple");
 console.log("hello");
-setProperty("WaterAppHeader", "background-color", "");
+setProperty("WaterAppHeader", "background-color", "white");
 
 onEvent("enButton", "click", function () {
     console.log("clicked english.");
     setText("WaterAppHeader", "Water Conservation Tips");
     setText("subHeader", " It's important that we all do our part to use less water. Click through this app for conservation ideas.");
+});
+onEvent("esButton", "click", function () {
+    console.log("clicked spanish.");
+    document.getElementById("Next").setAttribute("href", "newfaucet.html?lang=es");
+    setText("WaterAppHeader", "consejos de Conservacion del Aqua");
+    setText("subHeader", "Para conservar el agua es importante que todos pongamos de nuestra parte. Haz clic en esta aplicación para ver ideas de cómo hacerlo.");
 });
